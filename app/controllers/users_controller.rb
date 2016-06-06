c8c03d6a46b88a68c29d4f502f02c4f06bbdc25e
CallWayne <schlueter.niklas@gmail.com>
2016-06-06 18:14:42 +0200

    Seconde Commit
    verschiedene Methoden hinzugefügt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :pubkey]
+  skip_before_filter :verify_authenticity_token
+
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+  end
 
   def anmelden
     @user = User.find_by(login: params[:login])
@@ -11,11 +24,52 @@ class UsersController < ApplicationController
     end
   end
 
+  # POST /users
+  # POST /users.json
   def create
-
+    begin
+      @user = User.new(user_params)
+      if User.find_by_name(params[:name]) != nil
+        respond_to do |format|
+          format.json { render json: '{"status":"3"}'}
+        end
+      else
+        respond_to do |format|
+          if @user.save
+            format.json { render json: '{"status":"1"}'}
+          else
+            format.json { render json: '{"status":"2"}'}
+          end
+        end
+      end
+    rescue Exception
+      render json: '{"status":"99"}'
+    end
   end
 
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    if User.find_by_name(params[:id]) != nil then
+      @user = User.find_by_name(params[:id])
+    else
+      respond_to do |format|
+        format.json { render json: '{"status":"3"}'}
+      end
+    end
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:name, :public_key, :private_key_enc, :salt_master_key)
+  end
+
+  # Get /users/1/pubkey
   def pubkey
-
+    respond_to do |format|
+      format.json { render :pubkey, location: @user }
+    end
   end
+
+
 end
