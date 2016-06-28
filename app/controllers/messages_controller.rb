f8a61d2f0afe26adc592742269e6b4b25b5d55ad
CallWayne <schlueter.niklas@gmail.com>
2016-06-28 17:37:18 +0200

    noch ein paar Änderungen

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,43 +1,55 @@
 class MessagesController < ApplicationController
   require 'openssl'
+  skip_before_action :verify_authenticity_token
+  skip_before_filter :verify_authenticity_token
   
   def create
-    now = Time.now.to_i
-    if now - params[:timestamp].to_i <= 300
-      begin
-        @user = User.find_by(login: params[:login])
-        pubKey = OpenSSL::PKey::RSA.new @user.pubkey_user
-        pubKey.public_decrypt(Base64.decode64(params[:sig_service]))
-      rescue
-        head 400
-      end
-      Message.new(sender: params[:login],
-                  content_enc: params[:content_enc],
-                  iv: params[:iv],
-                  key_recipient_enc: params[:key_recipient_enc],
-                  sig_service: params[:sig_service],
-                  sig_recipient: params[:sig_recipient],
-                  recipient: params[:recipient])
+    if checktime == true
+      @user = User.find_by(login: params[:login])
+
+      pubKey = OpenSSL::PKey::RSA.new(Base64.decode64(@user.pubkey_user))
+
+      pubKey.public_decrypt(Base64.decode64(params[:sig_service]))
+
+      @message = Message.create(sender: params[:login],
+                              content_enc: params[:content_enc],
+                              iv: params[:iv],
+                              key_recipient_enc: params[:key_recipient_enc],
+                              sig_service: params[:sig_service],
+                              sig_recipient: params[:sig_recipient],
+                              recipient: params[:recipient])
+      render plain: "Test"
     else
-      head 400
+      head 400;
     end
   end
 
 
+
+
   def message
-    now = Time.now.to_i
-    if now - params[:timestamp].to_i <= 300
-      begin
-        @user = User.find_by(login: params[:login])
-        pubKey = OpenSSL::PKey::RSA.new @user.pubkey_user
-        pubKey.public_decrypt(params[:sig_service])
-      rescue
-        head 400
-      end
+    #now = Time.now.to_i
+    #if now - params[:timestamp].to_i <= 300
+    #  begin
+      #  @user = User.find_by(login: params[:login])
+     #   pubKey = OpenSSL::PKey::RSA.new @user.pubkey_user
+      #  pubKey.public_decrypt(params[:sig_service])
+     # rescue
+     #   head 400
+     # end
         message = Message.where(recipient: params[:login])
         render json: message.to_json(only: %w(sender content_enc iv key_recipient_enc sig_recipient created_at))
+    #else
+    #  head 400
+   # end
+  end
+
+  def checktime
+    timestamp = Time.now.to_i
+    if ((((timestamp - (params[:timestamp]).to_i) / 1.minute) <= 5))
+      return true
     else
-      head 400
+      return nil
     end
   end
 end
